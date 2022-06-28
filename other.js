22618a1d22febe3186717c4ffcae3c97b382e5f8
Md Isahaq Ali <isahaqali331065@gmail.com>
2022-06-28 20:56:00 +0600

    First commit

---
+++ other.js
@@ -0,0 +1,11 @@
+const area= r => 3.14*r*r;
+const circumference= r => 2*3.14*r;
+
+const msg= "hello from circle"
+
+
+//module.exports.area= area;
+module.exports.circumference= circumference;
+exports.area= area;
+
+exports.msg= msg;
